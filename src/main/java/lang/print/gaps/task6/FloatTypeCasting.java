37b579d5d68725b3b9420824531fee30010260fb
Bilbo3340 <vladik.elce1@gmail.com>
2022-08-30 18:32:32 +0300

    upt

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task6;
 
+import java.util.Scanner;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        System.out.println(Math.round(numberToBeRounded));
     }
 }
